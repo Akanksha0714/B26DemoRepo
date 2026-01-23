634352e686905e322d45e9f62aec5778e57ea51a
Arpitha Kudave <Arpithakudave@gmail.com>
2026-01-23 21:28:23 +0530

    Arpita- 01
    Co-authored-by: Arpitha <Arpitha@LAPTOP-68D4VICH>

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -23,12 +23,14 @@ namespace WebApplication.Controllers
             return View();
         }
 
+        public IActionResult ArpithaKudave()
+        {
+          return view();
+        }
         public IActionResult PruthwiGarale()
         {
             return View();
         }
-
-
         public IActionResult Privacy()
         {
             return View();
